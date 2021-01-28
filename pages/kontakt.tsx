e9acf0087b4b68d2beb15961b1e0d403090a8c1b
fhaenel <fh@agentur-brandmarker.de>
2021-01-28 12:52:16 +0100

    add contact page + info

---
+++ pages/kontakt.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import Layout, { siteTitle } from '../components/layout'
+
+export default function Kontakt() {
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle} | Kontakt</title>
+      </Head>
+      <h1>Kontakt</h1>
+      <div>
+        Adresse<br />
+        AK Theater am Campus<br />
+        Arbeitskreis des Studierendenparlaments<br />
+        Universität Bayreuth<br />
+        Universitätsstraße 30<br />
+        95440 Bayreuth<br />
+        Glasmittelbau, Zi. 1.42<br />
+
+        Ansprechpartner<br />
+        Lisa-Michelle Helten<br />
+        Donata Hörr<br />
+
+        E-Mail und Social Media Kanäle<br />
+        AK.TaC@Uni-Bayreuth.de<br />
+
+        Für aktuelle Informationen<br />
+        facebook.com/TaCBayreuth<br />
+        instagram.com/TacBayreuth<br />
+
+        Verantwortlich für die Redaktion: Jonas Würdinger
+      </div>
+    </Layout>
+  )
+}
