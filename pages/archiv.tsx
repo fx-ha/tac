61813c38c7dcb75738bb8c91fd48fb545576cc9a
Felix Ha <felixha@protonmail.com>
2021-02-01 21:36:57 +0100

    event list from api w/ getStaticProps

--- pages/archiv.tsx
+++ pages/archiv.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 
 import Layout, { siteTitle } from '../components/layout'
 
-export default function Archiv() {
+export default function Archiv({ events }) {
   return (
     <Layout>
       <Head>
@@ -10,6 +11,27 @@ export default function Archiv() {
       </Head>
       <h1>Archiv</h1>
       <div>Auflistung vergangener Aufführungen</div>
+      <div>
+        <ul>
+          {events.items.map((event: { id: number; title: string }) =>
+            <li key={event.id}>{event.title}</li>
+          )}
+        </ul>
+      </div>
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://127.0.0.1:8000/api/v2/pages/?child_of=4")
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 1 // revalidate once per second
+  }
+}
+
+// TODO getStaticPaths
--- pages/spielplan.tsx
+++ pages/spielplan.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 
 import Layout, { siteTitle } from '../components/layout'
 
-export default function Spielplan() {
+export default function Spielplan({ events }) {
   return (
     <Layout>
       <Head>
@@ -10,6 +11,28 @@ export default function Spielplan() {
       </Head>
       <h1>Spielplan</h1>
       <div>Auflistung aller Aufführungen für das Semester</div>
+      <div>
+        <ul>
+          {events.items.map((event: { id: number; title: string }) =>
+            <li key={event.id}>{event.title}</li>
+          )}
+        </ul>
+      </div>
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://127.0.0.1:8000/api/v2/pages/?child_of=3")
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 1 // revalidate once per second
+  }
+}
+
+// TODO getStaticPaths
+// TODO create event(listing) component
