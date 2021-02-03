e96c9ad55ac624f458d34b8a87338475173a7d34
Felix Haenel <felixha@protonmail.com>
2021-02-03 17:49:05 +0000

    Revert "next_public url"
    This reverts commit bb30f25ddc6d90a9ae12a611c61c0d640025cbce

--- pages/archiv/[id].tsx
+++ pages/archiv/[id].tsx
@@ -19,7 +19,7 @@ export default function Event({ event }) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}?child_of=4`)
+  const res = await fetch(`${process.env.API_URL}?child_of=4`)
   const events = await res.json()
   const paths = events.items.map((event: { id: string }) => `/archiv/${event.id}`)
 
@@ -27,7 +27,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${params.id as string}`)
+  const res = await fetch(`${process.env.API_URL}${params.id as string}`)
   const event = await res.json()
 
   return {
