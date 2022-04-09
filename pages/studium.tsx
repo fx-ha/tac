bc46a6d08cd49434a4aaa15f3cfb3a08b5d8da42
Felix Ha <felixha@protonmail.com>
2022-04-09 20:47:34 +0100

    feat: :sparkles: add dynamic study page

--- pages/studium.tsx
+++ pages/studium.tsx
@@ -1,135 +1,40 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import Layout, { siteTitle } from '../components/Layout'
 
-const Studium = () => {
+const Studium = ({study}: {study: any}) => {
   return (
     <Layout>
       <Head>
         <title>{siteTitle} | studieren</title>
       </Head>
-      <p>
-        An der Universität Bayreuth könnt ihr auch im Bereich Theater ein
-        Studium absolvieren!
-      </p>
-      <p>Die Studiengänge stellen wir euch hier vor:</p>
-      <h2 className="h5 mt-4 mb-2">Musiktheaterwissenschaft (B.A.)</h2>
-      <p>
-        Das Bachelorstudium der Musiktheaterwissenschaft bietet dir eine solide
-        Grundausbildung für verschiedene Berufe im Bereich Musiktheater
-        (Dramaturgie, Regie, Regieassistenz, Kulturadministration,
-        Musikjournalismus oder Musikverlagswesen).
-      </p>
-      <p>
-        Unter Musiktheater verstehen wir die unterschiedlichen Ausformungen des
-        europäischen Musiktheaters in Sparten Oper, Operette, Musical,
-        Tanztheater und Performance. Im Studiengang werden zunächst
-        Grundkenntnisse zur Geschichte, Analyse und Theorie musiktheatraler
-        Formen vermittelt. Diese können weiterführend nach eigenen
-        interessensschwerpunkten beispielsweise in Schauspiel, Tanz, Konzert und
-        Oper vertieft werden. Das Studium des B.A. Musiktheaterwissenschaft
-        vermittelt somit ein Grundverständnis für aufführungsbezogene Aspekte im
-        Musiktheater und darüber hinaus.
-      </p>
-      <p>
-        Zudem bieten wir Einblicke in die Praxis: im alltäglichen Unterricht
-        selbst, aber auch bei Exkursionen, Aufführungsbesuchen, in der
-        Zusammenarbeit mit Spezialistinnen und Spezialisten aus der Praxis und
-        bei selbständigen Praktika in musiktheaterbezogenen Betrieben.
-      </p>
-      <p>
-        Website:{' '}
-        <a
-          href="https://tinyurl.com/musiktheaterwissenschaft"
-          target="_blank"
-          rel="noreferrer"
-          className="text-reset"
-        >
-          https://tinyurl.com/musiktheaterwissenschaft
-        </a>
-      </p>
-
-      <h2 className="h5 mt-4 mb-2">Theater und Medien (B.A.)</h2>
-      <p>
-        Der Bachelorstudiengang &#34;Theater und Medien&#34; führt zwei
-        Disziplinen zusammen. Die Kombination von theater- und
-        medienwissen­schaftlichen Studieninhalten bietet eine solide Grundla­ge
-        nicht nur für ein weit gefächertes berufspraktisches Feld, sondern auch
-        für eine Fortführung des Studiums in Masterstudiengängen, etwa im
-        Bereich Journalismus, Dramaturgie, Drehbuch oder Medienkunst.
-      </p>
-      <p>
-        Der BA Theater und Medien zeichnet sich durch seinen starken
-        Anwendungsbezug aus. Hier werden Stücke und Kurzfilme von den
-        Studierenden selbst entwickelt und in eigenen Theater- und Medienlaboren
-        produziert. Eigeninitiative ist gefragt!
-      </p>
-      <p>
-        Die kulturellen, sozialen und historischen Funktionen von Theater und
-        Medien stehen im Mittelpunkt der Vorlesungen und Seminare. Die
-        Studierenden werden an die vielfältigen Erscheinungs­formen des Theaters
-        und der Medien in Geschichte und Gegenwart he­rangeführt; sie lernen
-        historische und aktuelle Entwick­lungen des Musik-, Sprech- und
-        Bewegungstheaters kennen. Sie machen sich zudem nicht nur mit
-        verschie­denen Medien- und Theatersparten vertraut, sondern erfahren
-        auch deren intermediale Vernetzungen im Bereich Medienkunst, Games,
-        Performance- und Body Art.
-      </p>
-      <p>
-        Website:{' '}
-        <a
-          href="https://tinyurl.com/theaterundmedien"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-reset"
-        >
-          https://tinyurl.com/theaterundmedien
-        </a>
-      </p>
-
-      <h2 className="h5 mt-4 mb-2">Theaterdidaktik / Darstellendes Spiel</h2>
-      <p>
-        Die Studiengänge Theaterdidaktik (Bachelor) und Darstellendes Spiel
-        (Lehramt) richten sich an Studierende, die an der Theaterarbeit mit
-        nicht-professionellen Darsteller/inne/n interessiert sind, also z. B.
-        mit Kindern und Jugendlichen, mit Erwachsenen, in altersgemischten
-        Gruppen, in interkulturellen oder inklusiven Kontexten.
-      </p>
-      <p>
-        Das Studium vermittelt sowohl die theoretischen als auch die praktischen
-        Qualifikationen, die angehende Spielleiter benötigen: <br />
-        Es ermöglicht den Erwerb theater­wissenschaftlicher, pädagogischer und
-        fachdidaktischer Kenntnisse und theater­praktischer Fähigkeiten, die die
-        eigene Theaterarbeit und die Gestaltung und Anleitung kreativer
-        szenischer Spielprozesse fundieren. Die einzelnen Fachteile sind
-        konzeptionell aufeinander bezogen und verknüpfen konsequent theoretische
-        Reflexion und praktische Erprobung.
-      </p>
-      <p>
-        Neben den Lehrveranstaltungen umfasst das Studium auch ein Praktikum,
-        das je nach Interesse an verschiedenen Bildungseinrichtungen absolviert
-        werden kann: z. B. im Schultheater, beim Kindermusical einer unserer
-        Partnerhochschulen, in der Abteilung Theaterpädagogik der umliegenden
-        Theaterhäuser, im Seniorenheim oder im Zentrum für Theater und
-        Integration/Inklusion Bayreuth.
-      </p>
-      <p>
-        Abgerundet wird das Studium durch ein selbstverantwortetes
-        theaterpraktisches Abschlussprojekt.
-      </p>
-      <p>
-        Website:{' '}
-        <a
-          href="https://tinyurl.com/theaterdidaktik"
-          target="_blank"
-          rel="noreferrer"
-          className="text-reset"
-        >
-          https://tinyurl.com/theaterdidaktik
-        </a>
-      </p>
+      <div>
+      {study?.body?.map((item: { id: string; value: string }) => (
+        <section
+          className="mb-2"
+          key={item.id}
+          dangerouslySetInnerHTML={{ __html: item.value }}
+        ></section>
+      ))}
+    </div>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    `${process.env.API_URL}?type=study.StudyIndexPage&fields=body`
+  )
+  const json = await res.json()
+  const study = json?.items?.[0]
+
+  return {
+    props: {
+      study,
+    },
+    revalidate: 10,
+  }
 }
 
 export default Studium
