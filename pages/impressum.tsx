1d719abefa0d0bf011c0207d11a22f8d065cab85
Felix Ha <felixha@protonmail.com>
2022-05-26 19:18:47 +0200

    feat: :speech_balloon: update imprint (#51)

--- pages/impressum.tsx
+++ pages/impressum.tsx
@@ -10,7 +10,9 @@ const Impressum = () => {
       </Head>
       <div>
         <h2>Angaben gemäß § 5 TMG:</h2>
-        Universität Bayreuth
+        Arbeitskreis Theater am Campus
+        <br />
+        Studierendenparlament
         <br />
         Universitätsstraße 30
         <br />
@@ -18,54 +20,25 @@ const Impressum = () => {
       </div>
       <div className="mt-3">
         <h2>Vertreten durch:</h2>
-        Der Präsident der Universität Bayreuth
-        <br />
-        Herrn Professor Dr. Stefan Leible
-        <br />
-        Universitätsstraße 30
-        <br />
-        95447 Bayreuth
-        <br />
-        Der Kanzler der Universität Bayreuth
-        <br />
-        Herrn Dr. Markus Zanner
-        <br />
-        Universitätsstraße 30
-        <br />
-        95447 Bayreuth
+        Samuel Niese & Julia Schutt - Vorstand des Arbeitskreises Theater am Campus
       </div>
       <div className="mt-3">
         <h2>Kontakt:</h2>
-        Tel.: 0921 / 55-0
-        <br />
-        Fax: 0921 / 55-5290
-        <br />
-        E-Mail:info@uni-bayreuth.de
-        <br />
-      </div>
-      <div className="mt-3">
-        <h2>Umsatzsteuer-ID:</h2>
-        Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
-        <br />
-        DE 811 264 317
-      </div>
-      <div className="mt-3">
-        <h2>Aufsichtsbehörde:</h2>
-        Bayerisches Staatsministerium für Wissenschaft und Kunst
+        <a
+          className="text-reset"
+          href="mailto:Ak.tac@uni-bayreuth.de"
+          title="e-mail an ak tac senden"
+        >
+          Ak.tac@uni-bayreuth.de
+        </a>
       </div>
       <div className="mt-3">
         <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
-        Universität Bayreuth
+        Daniel Schauf
         <br />
-        Stabsabteilung, Presse, Marketing und Kommunikation
+        Bornholmer Straße 78
         <br />
-        Universitätsstraße 30
-        <br />
-        95447 Bayreuth
-        <br />
-        E-Mail: angela.danner@uni-bayreuth.de
-        <br />
-        Fotos: Soweit nicht anders angegeben: Copyright Universität Bayreuth.
+        10439 Berlin
       </div>
     </Layout>
   )
